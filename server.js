425b2090b355549c9c105f130fa70883058f2ab2
Hector Romo <hectorommmo@gmail.com>
2022-09-08 21:47:21 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// const {
+//     animals
+// } = require('./data/animals.json');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const fs = require('fs');
+const path = require('path');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({
+    extended: true
+}));
+// parse incoming JSON data
+app.use(express.json());
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
